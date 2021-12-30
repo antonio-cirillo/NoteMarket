537438a53bb70553410108cdf5f4ebeff75e0f59
Antonio CIrillo <dev.cirillo@gmail.com>
2021-12-30 01:02:48 +0100

    added status parameter

--- Serverless/models.js
+++ Serverless/models.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-    name: String,
-    surname: String
+    email: { type: String, required: true},
+    password: { type: String, required: true},
+    name: { type: String, required: true},
+    surname: { type: String, required: true},
+    status: { type: String, required: true}
 }, {
     versionKey: false
 });
