b11ff985caf45d0d7e9af7c881f1e5b431a9bc96
Antonio CIrillo <dev.cirillo@gmail.com>
2021-12-30 15:50:50 +0100

    change directory

---
+++ Web App/router/login.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Declare email and password regex
+const EMAIL_REGEX = /^[a-zA-Z0-9]{1}[.!#$%&'*+/=?^_`{|}~a-zA-Z0-9-]{0,99}@[a-zA-Z0-9]{1,46}\.[a-zA-Z]{2,4}$/;
+const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$/;
+
+export function getLogin(req, res) {
+
+    res.render('login', {
+        error: false
+    });
+
+}
+
+export function postLogin(req, res) {
+    
+    const { email, password } = req.body;
+    
+    // If email or password doesn't match with pattern 
+    if (!EMAIL_REGEX.test(email) || !PASSWORD_REGEX.test(password)) {
+        res.render('login', {
+            error: 'genericError'
+        });
+    } 
+    // Execute login serverless
+    else {
+        axios.post('http://localhost:7071/api/login', {
+            email: email,
+            password: password
+        }).then((response) => {
+            // Check if there is an error
+            if (response.data.error) {
+                res.render('login', {
+                    error: response.data.error
+                });
+            } 
+            // Login successful
+            else {
+                console.log(response.data);
+            }
+        }, (error) => {
+            res.render('login', {
+                error: 'genericError'
+            });
+        });
+    }
+    
+}
