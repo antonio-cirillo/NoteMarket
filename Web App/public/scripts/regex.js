6493961b97ac9f0377e4f1bba7e1e5d4e4a271af
Antonio CIrillo <dev.cirillo@gmail.com>
2021-12-30 17:46:10 +0100

    added name/surname regex

--- Web App/public/scripts/regex.js
+++ Web App/public/scripts/regex.js
@@ -1,3 +1,5 @@
 const EMAIL_REGEX = /^[a-zA-Z0-9]{1}[.!#$%&'*+/=?^_`{|}~a-zA-Z0-9-]{0,99}@[a-zA-Z0-9]{1,46}\.[a-zA-Z]{2,4}$/;
 
 const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$/;
+
+const NAME_SURNAME_REGEX = /^[A-Za-z][a-zA-Z\s]{1,13}[A-Za-z]$/;
