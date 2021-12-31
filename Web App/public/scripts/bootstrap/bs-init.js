e6bba582030232893f05f7159d1c5d497c902e22
Antonio CIrillo <dev.cirillo@gmail.com>
2021-12-31 20:27:09 +0100

    modify script

--- Web App/public/scripts/bootstrap/bs-init.js
+++ Web App/public/scripts/bootstrap/bs-init.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
-	$('[data-bs-hover-animate]')
-		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
-		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
-});
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
+	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
+		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
+	});
+}, false);
