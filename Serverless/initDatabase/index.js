643de1605190d1f4418f7730fe4d1be4e50d0573
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-01 17:53:25 +0100

    initDatabase serverless

---
+++ Serverless/initDatabase/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Users, Catalog } = require('../models.js');
+const { USERS, CATALOG } = require('../database.js');
+
+require('dotenv').config();
+
+module.exports = async function (context, req) {
+    
+    try {
+
+        // Connect to database
+        mongoose.connect(process.env['MONGO_URI']);
+
+        // Create user
+        if (await Users.countDocuments() == 0) {
+            await Users.insertMany(USERS);
+        }
+
+        // Create catalog
+        if (await Catalog.countDocuments() == 0) {
+            await Catalog.insertMany(CATALOG);
+        }
+
+        // Return confirm
+        context.res = { body: { flag: true } };
+
+    } catch (error) {
+        console.log(error);
+        // Return error
+        context.res = { body: { flag: false } };
+    }
+
+}
