873a4aad356b1d4d510a3c059ce9620fc14c594f
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-01 17:53:52 +0100

    added Catalog schema

--- Serverless/models.js
+++ Serverless/models.js
@@ -1,20 +1,34 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true},
-    password: { type: String, required: true},
-    name: { type: String, required: true},
-    surname: { type: String, required: true},
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
     dob: { type: Date, required: true },
-    status: { type: String, required: true},
+    status: { type: String, required: true },
     activeToken: String,
     activeExpires: Date
 }, {
     versionKey: false
 });
 
+const catalogSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    file: { type: String, required: true },
+    emailVendor: { type: String, required: true },
+    status: { type: String, required: true },
+    emailModerator: String
+}, {
+    versionKey: false
+});
+
 module.exports = {
 
-    Users: mongoose.model('Users', userSchema)
+    Users: mongoose.model('Users', userSchema),
+    Catalog: mongoose.model('Catalog', catalogSchema)
 
 }
