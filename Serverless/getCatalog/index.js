559c6b108845c56a7d7cb8d9fc541fa9454cd879
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-01 18:03:29 +0100

    getCatalog serverless

---
+++ Serverless/getCatalog/index.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { Catalog } = require('../models.js');
+
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+module.exports = async function (context, req) {
+
+    // Get title
+    const title = (req.query.title || (req.body && req.body.title));
+
+    try {
+
+        // Connect to database
+        mongoose.connect(process.env['MONGO_URI']);
+
+        // Declare catalog object
+        let catalog;
+
+        // Query database
+        if (title) {
+            // Use filter
+            catalog = await Catalog.find({
+                "title": { "$regex": title, "$options": "i" },
+                "status": "verified"
+            });
+        } else {
+            // Get all
+            catalog = await Catalog.find({
+                "status": "verified"
+            });
+        }
+
+        // Return catalog
+        context.res = { body: catalog };
+
+    } catch (error) {
+        // Return error
+        context.res = { body: { error: true } };
+        return;
+    }
+
+}
