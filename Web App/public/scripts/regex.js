daa279d9264aa680a1db28ea1f0b44a58f3b26d4
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-02 17:00:59 +0100

    added comment regex

--- Web App/public/scripts/regex.js
+++ Web App/public/scripts/regex.js
@@ -3,3 +3,5 @@ const EMAIL_REGEX = /^[a-zA-Z0-9]{1}[.!#$%&'*+/=?^_`{|}~a-zA-Z0-9-]{0,99}@[a-zA-
 const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$/;
 
 const NAME_SURNAME_REGEX = /^[A-Za-z][a-zA-Z\s]{1,13}[A-Za-z]$/;
+
+const COMMENT_REGEX = /[a-zA-Z0-9@=\-'"]{3,256}/
