5137d15cf4d801e57e05d5d8294a68f400ede20d
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-02 21:51:44 +0100

    change position of getAddToCart method

--- Web App/router/catalog.js
+++ Web App/router/catalog.js
@@ -105,7 +105,7 @@ export async function getItem(req, res) {
     if (req.session.user) {
         object.logged = true;
         if (req.session.user.itemsBuyed.includes(_id) ||
-                req.session.user.itemSelling.includes(_id)) {
+                req.session.user.itemsSelling.includes(_id)) {
                     object.isOwned = true;
                 }
         if (req.session.cart.includes(_id)) {
@@ -121,47 +121,5 @@ export async function getItem(req, res) {
     }
 
     res.render('item', object);
-
-}
-
-export async function getAddToCart(req, res) {
-
-    // Get id
-    const _id = req.body._id;
-
-    // Call serverless for get item
-    try {
-
-        const response = await axios.post(process.env.URL_FUNCTION_GET_ITEM, { _id: _id });
-        // Check if item is not verified
-        if (response.data.status == "notVerified") {
-            res.status(404).json({ error: true });
-            return;
-        }
-        // Check if there is an error
-        if (response.data.error) {
-            res.status(404).json({ error: true });
-            return;
-        } 
-
-    } catch (error) {
-        res.status(404).json({ error: true });
-        return;
-    }
-    
-    // Check if item is or not inside cart
-    const index = req.session.cart.indexOf(_id);
-    // If item is not inside cart
-    if (index == -1) {
-        req.session.cart.push(_id);
-        req.session.save();
-        res.status(200).json({ error: false });
-        return;
-    } else {
-        req.session.cart.splice(index, 1);
-        req.session.save();
-        res.status(200).json({ error: false });
-        return;
-    }
 
 }
