d48e61a76c848c218d14f372a83a4d0becafeeee
Adipiz <a.dipierno99@gmail.com>
2022-01-04 20:58:46 +0100

    bug fix

--- Telegram Bot/dialogs/loginDialog.js
+++ Telegram Bot/dialogs/loginDialog.js
@@ -3,10 +3,10 @@ const { ComponentDialog, DialogSet, DialogTurnStatus, TextPrompt, WaterfallDialo
 const { CancelAndHelpDialog } = require('./cancelAndHelpDialog');
 
 const MAIN_WATERFALL_DIALOG = 'loginWaterfallDialog';
-const TEXT_PROMPT = 'loginTextprompt';
+const TEXT_PROMPT = 'TextPrompt';
 const EMAIL_REGEX = /^[a-zA-Z0-9]{1}[.!#$%&'*+/=?^_`{|}~a-zA-Z0-9-]{0,99}@[a-zA-Z0-9]{1,46}\.[a-zA-Z]{2,4}$/;
 const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$/;
-const userDetails = null;
+var userDetails = null;
 
 class LoginDialog extends CancelAndHelpDialog {
     constructor(id) {
@@ -25,29 +25,33 @@ class LoginDialog extends CancelAndHelpDialog {
 
     //Prompt the user to enter its credentials
     async emailStep(stepContext) {
+        console.log('[LoginDialog]: emailStep');
+        //Initialize userDetails and accountDetails objects
+        console.log('[LoginDialog]: emailStep -> Inizializzo userDetails');
         userDetails = stepContext.options;
+        console.log('[LoginDialog]: emailStep -> Inizializzo accountDetails');
         stepContext.values.accountDetails = {email: null, password: null};
 
-        if (!accountDetails.email) {
-            const messageText = 'Enter your email';
+        //Check if the email field is empty and prompt for a valid email
+        //otherwise the dialog continues with the provided email
+        if (!stepContext.values.accountDetails.email) {
+            console.log('[LoginDialog]: emailStep -> email non presente, creo il prompt');
+            const messageText = 'Inserisci la tua email';
             const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
-
-            const repromptMessageText = "I'm sorry, a valid email is required to continue";
-            const repromptMessage = MessageFactory.text(repromptMessageText, repromptMessageText, InputHints.ExpectingInput);
-
+            
             return await stepContext.prompt(TEXT_PROMPT, { 
-                prompt: msg, 
-                retryPrompt: msg
+                prompt: msg
             });
         }
 
-        return await stepContext.next();
+        return await stepContext.next(stepContext.values.accountDetails.email);
     }
 
     async checkEmailStep(stepContext){
+        console.log('[LoginDialog]: checkEmailStep');
         //If test fails reprompt the user for a new email
         if(!EMAIL_REGEX.test(stepContext.result)){
-            const repromptMessageText = "I'm sorry, a valid email is required to continue";
+            const repromptMessageText = "Mi dispiace, per eseguire il login è richiesta una email valida";
             const repromptMessage = MessageFactory.text(repromptMessageText, repromptMessageText, InputHints.ExpectingInput);
 
             return await stepContext.prompt(TEXT_PROMPT, { prompt: repromptMessage });
@@ -56,20 +60,22 @@ class LoginDialog extends CancelAndHelpDialog {
     }
 
     async passwordStep(stepContext) {
+        console.log('[LoginDialog]: passwordStep');
         stepContext.values.accountDetails.email = stepContext.result;
 
-        if (!accountDetails.password) {
-            const messageText = 'Enter your password';
+        if (!stepContext.values.accountDetails.password) {
+            const messageText = 'Inserisci la tua password';
             const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
             return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
         }
-        return await stepContext.next();
+        return await stepContext.next(stepContext.values.accountDetails.password);
     }
 
     async checkPasswordStep(stepContext){
+        console.log('[LoginDialog]: checkPasswordStep');
         //If test fails reprompt the user for a new password
         if(!PASSWORD_REGEX.test(stepContext.result)){
-            const repromptMessageText = "La password inserita non rispetta il formato richiesto";
+            const repromptMessageText = "La password inserita non rispetta il formato richiesto, riprova";
             const repromptMessage = MessageFactory.text(repromptMessageText, repromptMessageText, InputHints.ExpectingInput);
 
             return await stepContext.prompt(TEXT_PROMPT, { prompt: repromptMessage });
@@ -78,6 +84,7 @@ class LoginDialog extends CancelAndHelpDialog {
     }
 
     async loginStep(stepContext) {
+        console.log('[LoginDialog]: loginStep');
         stepContext.values.accountDetails.password = stepContext.result;
         try{
             const response = await axios.post(process.env.URL_FUNCTION_LOGIN, {
@@ -92,7 +99,8 @@ class LoginDialog extends CancelAndHelpDialog {
             }
         }catch(error){}
 
-        //TODO: put response.data in userDetails object
+        //Put response.data in userDetails object
+        userDetails = response.data
         return await stepContext.endDialog(response.data);
     }
 }
