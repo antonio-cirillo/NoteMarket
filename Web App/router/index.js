eaee817b34f98fa43c19a82609f11f926b6a58a6
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-04 18:03:00 +0100

    added render home page

--- Web App/router/index.js
+++ Web App/router/index.js
@@ -2,7 +2,15 @@ export function getIndex(req, res) {
     
     // If user is logged 
     if (req.session.user) {
-        console.log(req.session.user);
+        if (req.session.moderator) {
+            console.log("You are moderator");
+        } else {
+            res.render('userHome', {
+                error: (req.query.error) ? req.query.error : '',
+                confirm: (req.query.confirm) ? req.query.confirm : '',
+                user: req.session.user
+            })
+        }
     } 
     // If user is not logged
     else {
