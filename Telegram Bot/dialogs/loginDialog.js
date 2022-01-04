fc350d04300e8fefc7328be98f698adc525568cb
Andrea Di Pierno <a.dipierno99@gmail.com>
2022-01-04 03:29:39 +0100

    minor bug fix

--- Telegram Bot/dialogs/loginDialog.js
+++ Telegram Bot/dialogs/loginDialog.js
@@ -6,7 +6,7 @@ const MAIN_WATERFALL_DIALOG = 'loginWaterfallDialog';
 const TEXT_PROMPT = 'loginTextprompt';
 const EMAIL_REGEX = /^[a-zA-Z0-9]{1}[.!#$%&'*+/=?^_`{|}~a-zA-Z0-9-]{0,99}@[a-zA-Z0-9]{1,46}\.[a-zA-Z]{2,4}$/;
 const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$/;
-const userDetails;
+const userDetails = null;
 
 class LoginDialog extends CancelAndHelpDialog {
     constructor(id) {
