56f8bfd7b29eb5217858c4a990abd6d30285fc79
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-05 18:17:31 +0100

    check if moderator is not logged

---
+++ Web App/interceptors/isNotModeratorLogged.js
@@ -0,0 +1,17 @@
+export default (req, res, next) =>  {
+    // If user are logged
+    if (req.session.user) {
+        // If moderator are logged
+        if (!req.session.user.moderator) {
+            // Redirect to home page
+            res.redirect('/');
+            return;
+        }
+    } else {
+        // Redirect to home page
+        res.redirect('/');
+        return;
+    }
+    
+    next();
+}
