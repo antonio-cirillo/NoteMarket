6d4be0591469eb219d6e4aea7564354b34cfc1d3
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-05 02:34:33 +0100

    added new regex

--- Web App/public/scripts/regex.js
+++ Web App/public/scripts/regex.js
@@ -5,3 +5,9 @@ const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,16}$/;
 const NAME_SURNAME_REGEX = /^[A-Za-z][a-zA-Z\s]{1,13}[A-Za-z]$/;
 
 const COMMENT_REGEX = /[a-zA-Z0-9@=\-'"]{3,256}/
+
+const TITLE_REGEX = /[a-zA-Z0-9@=\-'"]{3,50}/
+
+const PRICE_REGEX = /(0|([1-9][0-9]*))(\.[0-9]+)?/
+
+const DESCRIPTION_REGEX = /[a-zA-Z0-9@=\-'"]{3,512}/
