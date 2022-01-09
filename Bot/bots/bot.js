828a5a18477e6ea96b8bb7a934a74d5b5fe726ec
Antonio CIrillo <dev.cirillo@gmail.com>
2022-01-09 21:32:05 +0100

    initial commit

---
+++ Bot/bots/bot.js
@@ -0,0 +1,49 @@
+const { ActivityHandler } = require('botbuilder');
+
+class NoteMarketBot extends ActivityHandler {
+    constructor(conversationState, userState, dialog) {
+        super();
+        if (!conversationState) throw new Error('[DialogBot]: Missing parameter. conversationState is required');
+        if (!userState) throw new Error('[DialogBot]: Missing parameter. userState is required');
+        if (!dialog) throw new Error('[DialogBot]: Missing parameter. dialog is required');
+
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+        this.dialogState = this.conversationState.createProperty('DialogState');
+
+		this.onMembersAdded(async (context, next) => {
+			const membersAdded = context.activity.membersAdded;
+			for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+				if (membersAdded[cnt].id !== context.activity.recipient.id) {
+					await context.sendActivity(
+						`Benvenuto in NoteMarket! Come posso aiutarti?`,
+					);
+					await this.dialog.run(context, this.dialogState);
+				}
+			}
+
+			await next();
+		});
+
+        this.onMessage(async (context, next) => {
+            console.log('Running dialog with Message Activity.');
+
+            // Run the Dialog with the new message Activity.
+            await this.dialog.run(context, this.dialogState);
+
+            await next();
+        });
+    }
+
+    async run(context) {
+        await super.run(context);
+
+        // Save any state changes. The load happened during the execution of the Dialog.
+        await this.conversationState.saveChanges(context, false);
+        await this.userState.saveChanges(context, false);
+    }
+
+}
+
+module.exports.NoteMarketBot = NoteMarketBot;
