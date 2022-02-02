1eb1002a7710d08dc2d0526468351151afe3796a
Antonio CIrillo <dev.cirillo@gmail.com>
2022-02-02 19:28:43 +0100

    getItemsBuyed serverless

---
+++ Serverless/getItemsBuyed/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Users, Catalog } = require('../models.js');
+
+require('dotenv').config();
+
+module.exports = async function (context, req) {
+
+    // Get email
+    const email = (req.query.email || (req.body && req.body.email));
+
+    try {
+
+        // Connect to database
+        await mongoose.connect(process.env['MONGO_URI']);
+
+        // Get user from database by email
+        const user = await Users.findOne({
+            email: email
+        });
+
+        const items = [];
+        // Get items buyed
+        for (const _id of user.itemsBuyed) {
+            items.shift(await Catalog.findById(_id).exec());
+        }
+
+        // Return items
+        context.res = { body: items };
+
+    } catch (error) {
+        // Return error
+        context.res = { body: { error: true } };
+        return;
+    }
+
+}
