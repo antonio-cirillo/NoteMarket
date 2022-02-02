828c2e9146cb323b2beb3fbf12f5c932ee9cf6b5
Adipiz <a.dipierno99@gmail.com>
2022-02-02 18:10:26 +0100

    Changed login method

--- Telegram Bot/bots/bot.js
+++ Telegram Bot/bots/bot.js
@@ -1,5 +1,9 @@
-const { ActivityHandler } = require('botbuilder');
+const { ActivityHandler, CardFactory } = require('botbuilder');
 const { QnAMaker } = require('botbuilder-ai');
+
+const CONVERSATION_DATA_PROPERTY = 'CONVERSATION_DATA_PROPERTY';
+const USER_PROFILE_PROPERTY = 'USER_PROFILE_PROPERTY';
+const WELCOME_CARD = require('../cards/welcomeCard.json');
 
 class NoteMarketBot extends ActivityHandler {
     constructor(conversationState, userState, dialog, QnAConfig, qnaOptions) {
@@ -16,14 +20,33 @@ class NoteMarketBot extends ActivityHandler {
         this.userState = userState;
         this.dialog = dialog;
         this.dialogState = this.conversationState.createProperty('DialogState');
+        
+        // Create the state property accessors for the conversation data and user profile.
+        this.conversationDataAccessor = conversationState.createProperty(CONVERSATION_DATA_PROPERTY);
+        this.userProfileAccessor = userState.createProperty(USER_PROFILE_PROPERTY);
+
 
 		this.onMembersAdded(async (context, next) => {
 			const membersAdded = context.activity.membersAdded;
 			for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
 				if (membersAdded[cnt].id !== context.activity.recipient.id) {
-					await context.sendActivity(
-						`Benvenuto in NoteMarket! Come posso aiutarti?`,
-					);
+                    var base = 'Per utilizzare il nostro Bot devi effettuare il pairing tra il tuo account e Telegram:';
+                    var instr1 = `  - Copia il seguente ID: ${membersAdded[cnt].id}`;
+                    var instr2 = '  - Accedi al nostro sito tramite il link in basso;';
+                    var instr3 = '  - Inserisci l\'ID all\'interno del tuo profilo e premi il pulsante \'Salva\'';
+                    WELCOME_CARD.actions[0].url = process.env.YOUR_DOMAIN + '/login';
+                    WELCOME_CARD.body[2].text = base;
+                    WELCOME_CARD.body[3].text = instr1;
+                    WELCOME_CARD.body[4].text = instr2;
+                    WELCOME_CARD.body[5].text = instr3;
+                    await context.sendActivity({
+                        text: '',
+                        attachments: [CardFactory.adaptiveCard(WELCOME_CARD)]
+                    });
+                    const userProfile = await this.userProfileAccessor.get(context, {});
+                    userProfile.id = membersAdded[cnt].id;
+                    userProfile.isUserLoggedIn = false;
+                    await this.userState.saveChanges(context, false);
 					await this.dialog.run(context, this.dialogState);
 				}
 			}
@@ -33,11 +56,21 @@ class NoteMarketBot extends ActivityHandler {
 
         this.onMessage(async (context, next) => {
             console.log('Running main dialog with Message Activity.');
+            const userProfile = await this.userProfileAccessor.get(context, {});
+            if(!userProfile.isUserLoggedIn){
+                WELCOME_CARD.actions[0].url = process.env.YOUR_DOMAIN + '/login';
+                await context.sendActivity({
+                    text: '',
+                    attachments: [CardFactory.adaptiveCard(WELCOME_CARD)]
+                });
+                await next();
+            }
+            else{
+                // Run the Dialog with the new message Activity.
+                await this.dialog.run(context, this.dialogState);
 
-            // Run the Dialog with the new message Activity.
-            await this.dialog.run(context, this.dialogState);
-
-            await next();
+                await next();
+            }
         });
     }
 
