8ea91e267ca233c9fe1a24bb949e0817fa30bc0a
Adipiz <a.dipierno99@gmail.com>
2022-02-03 01:11:54 +0100

    fix bug

--- Telegram Bot/bots/bot.js
+++ Telegram Bot/bots/bot.js
@@ -5,6 +5,10 @@ const CONVERSATION_DATA_PROPERTY = 'CONVERSATION_DATA_PROPERTY';
 const USER_PROFILE_PROPERTY = 'USER_PROFILE_PROPERTY';
 const WELCOME_CARD = require('../cards/welcomeCard.json');
 
+const axios = require('axios');
+require('dotenv').config();
+
+
 class NoteMarketBot extends ActivityHandler {
     constructor(conversationState, userState, dialog, QnAConfig, qnaOptions) {
         super();
@@ -28,42 +32,67 @@ class NoteMarketBot extends ActivityHandler {
 
 		this.onMembersAdded(async (context, next) => {
 			const membersAdded = context.activity.membersAdded;
+
 			for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-				if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    var base = 'Per utilizzare il nostro Bot devi effettuare il pairing tra il tuo account e Telegram:';
-                    var instr1 = `  - Copia il seguente ID: ${membersAdded[cnt].id}`;
-                    var instr2 = '  - Accedi al nostro sito tramite il link in basso;';
-                    var instr3 = '  - Inserisci l\'ID all\'interno del tuo profilo e premi il pulsante \'Salva\'';
+                if (membersAdded[cnt].id != context.activity.recipient.id) {
                     WELCOME_CARD.actions[0].url = process.env.YOUR_DOMAIN + '/login';
-                    WELCOME_CARD.body[2].text = base;
-                    WELCOME_CARD.body[3].text = instr1;
-                    WELCOME_CARD.body[4].text = instr2;
-                    WELCOME_CARD.body[5].text = instr3;
                     await context.sendActivity({
-                        text: '',
                         attachments: [CardFactory.adaptiveCard(WELCOME_CARD)]
+                    });
+                    await context.sendActivity({
+                        text: 'Copia il seguente token: ' + membersAdded[cnt].id
                     });
                     const userProfile = await this.userProfileAccessor.get(context, {});
                     userProfile.id = membersAdded[cnt].id;
                     userProfile.isUserLoggedIn = false;
                     await this.userState.saveChanges(context, false);
-					await this.dialog.run(context, this.dialogState);
-				}
+                }
 			}
 
 			await next();
-		});
+        });
 
         this.onMessage(async (context, next) => {
-            console.log('Running main dialog with Message Activity.');
             const userProfile = await this.userProfileAccessor.get(context, {});
+
+            if(!userProfile.id){
+                userProfile.id = context.activity.from.id;
+                userProfile.isUserLoggedIn = false;
+                await this.userState.saveChanges(context, false);
+            }
+
             if(!userProfile.isUserLoggedIn){
-                WELCOME_CARD.actions[0].url = process.env.YOUR_DOMAIN + '/login';
-                await context.sendActivity({
-                    text: '',
-                    attachments: [CardFactory.adaptiveCard(WELCOME_CARD)]
-                });
-                await next();
+                try{
+                    const response = await axios.post(process.env.URL_FUNCTION_CHECK_TELEGRAM_TOKEN, {telegramToken: userProfile.id});
+    
+                    if (!response.data.error) {
+                        userProfile.isUserLoggedIn = true;
+                        userProfile.user = response.data;
+                        await this.userState.saveChanges(context, false);
+                        // Run the Dialog with the new message Activity.
+                        await this.dialog.run(context, this.dialogState);
+
+                        await next();
+                    }
+                    else{
+                        WELCOME_CARD.actions[0].url = process.env.YOUR_DOMAIN + '/login';
+                        await context.sendActivity({
+                            attachments: [CardFactory.adaptiveCard(WELCOME_CARD)]
+                        });
+                        await context.sendActivity({
+                            text: 'Copia il seguente token: ' + userProfile.id
+                        });  
+                    }
+                }
+                catch(error){
+                    WELCOME_CARD.actions[0].url = process.env.YOUR_DOMAIN + '/login';
+                    await context.sendActivity({
+                        attachments: [CardFactory.adaptiveCard(WELCOME_CARD)]
+                    });
+                    await context.sendActivity({
+                        text: 'Copia il seguente token: ' + userProfile.id
+                    });  
+                } 
             }
             else{
                 // Run the Dialog with the new message Activity.
@@ -81,7 +110,7 @@ class NoteMarketBot extends ActivityHandler {
         await this.conversationState.saveChanges(context, false);
         await this.userState.saveChanges(context, false);
     }
-
 }
 
 module.exports.NoteMarketBot = NoteMarketBot;
+module.exports.userProfileAccessor = this.userProfileAccessor;
